bb64b83e7abd0685c45642e55283acfa91dcc100
subhamghimire <subham.ghimire26@gmail.com>
2021-11-30 18:54:29 +0545

    minor

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
      * You can enable CORS for 1 or multiple paths.
      * Example: ['api/*']
      */
-    'paths' => ['*'],
+    'paths' => ['api/*', 'api/score', 'sanctum/csrf-cookie', 'login', 'logout'],
 
     /*
     * Matches the request method. `['*']` allows all methods.
