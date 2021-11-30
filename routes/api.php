306a98c4e0c1571343b5efbaa7e45cc6f077e6bb
subhamghimire <subham.ghimire26@gmail.com>
2021-11-30 18:01:51 +0545

    aut

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('login', 'Auth\LoginController@login');
 Route::post('register','Auth\RegisterController@store')->name('register');
 
-Route::group(['middleware'=>'auth:sanctum'],function () {
+Route::group(['middleware'=>'auth'],function () {
     Route::get('user', 'Auth\LoginController@user');
     Route::post('score','Api\ScoreController@score');
     Route::get('highScore','Api\ScoreController@highScore');
