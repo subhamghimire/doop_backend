dd489a28540a9a32182f8554c57e77ad8bfb9a99
subhamghimire <subham.ghimire26@gmail.com>
2021-12-01 09:39:44 +0545

    remove token field from fac

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,7 +28,6 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => \Hash::make('password@123'),
             'remember_token' => Str::random(10),
-            'token' => "testestest"
         ];
     }
 
